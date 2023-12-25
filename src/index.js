9b45a11d7234246a7e99e0ee7600cf8e450e58ca
Aldiana <hotaldiana@gmail.com>
2023-12-24 21:17:27 -0500

    basic front end started

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import Component from './component'; // Adjust the path based on your project structure
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Your Recipe App</h1>
+      <Component />
+    </div>
+  );
+}
+
+export default App;
